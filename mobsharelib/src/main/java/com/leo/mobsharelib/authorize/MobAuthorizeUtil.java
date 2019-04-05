70fefc161d8dfd70405de3c2cc34ad9a1745e161
leo <448740656@qq.com>
2019-04-05 17:28:11 +0800

    修改依赖

--- mobsharelib/src/main/java/com/leo/mobsharelib/authorize/MobAuthorizeUtil.java
+++ mobsharelib/src/main/java/com/leo/mobsharelib/authorize/MobAuthorizeUtil.java
@@ -1,16 +1,22 @@
 package com.leo.mobsharelib.authorize;
 
-import android.support.annotation.NonNull;
+
+import android.util.Log;
 
 import cn.sharesdk.framework.Platform;
 import cn.sharesdk.framework.PlatformActionListener;
 
 public final class MobAuthorizeUtil {
+    private static final String TAG = MobAuthorizeUtil.class.getSimpleName();
 
-    public static void authorize(@NonNull Platform plat, @NonNull PlatformActionListener listener) {
+    public static void authorize(Platform plat, PlatformActionListener listener) {
         // 关闭SSO授权(true)
-        plat.SSOSetting(false);
-        plat.setPlatformActionListener(listener);
-        plat.showUser(null);
+        if (null != plat && null != listener) {
+            plat.SSOSetting(false);
+            plat.setPlatformActionListener(listener);
+            plat.showUser(null);
+        } else {
+            Log.e(TAG, "Platform or PlatformActionListener is null");
+        }
     }
 }
